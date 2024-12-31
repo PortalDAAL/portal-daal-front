185d35ba25c66a08452e2356b2d448117f0a3ab3
leirdan <uouleirdan@gmail.com>
2024-12-31 13:08:00 -0300

    fix: mapeando corretamente campo de Event

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,10 +1,17 @@
+export type EventPoster = {
+  url: string;
+  size: number;
+  name: string;
+  ext?: string;
+};
+
 export interface Event {
   readonly id: number;
   title: string;
   date: Date;
   description: string;
   place: string;
-  poster: File | string;
+  poster: EventPoster;
   startInscriptions?: Date;
   endInscriptions?: Date;
   active: boolean;
