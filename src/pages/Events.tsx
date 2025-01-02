d5355d9a2ef81c9097cb1613fcfb9fa94c2844e3
leirdan <uouleirdan@gmail.com>
2025-01-02 09:19:19 -0300

    fix: removendo botão de cadastrar evento

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,24 +1,11 @@
 import React from "react";
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
-import { useUser } from "../UserContext";
 import EventsList from "../components/events/events-list";
 import EventsTitle from "../components/events/events-title";
 
 const Events = (): React.ReactElement => {
-  const { user } = useUser();
-
   return (
     <div className="container">
-      {user && (
-        <>
-          <Link to={"/addEvent"}>
-            <Button> Cadastrar Evento </Button>
-          </Link>
-        </>
-      )}
       <EventsTitle />
-
       <EventsList />
     </div>
   );
