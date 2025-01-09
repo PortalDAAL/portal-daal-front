b0ec8d2c6bcba04c0c72a72bccf8742160fbc50c
Mariana Timbó <mariana.timbo.114@ufrn.edu.br>
2025-01-09 12:12:03 -0300

    feat: feita parte de membros daal

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Divider } from "@mui/material";
+import { Box, Divider} from "@mui/material";
+import MemberProfile from "../components/about/member-profile";
 
 const About = (): React.ReactElement => {
   return (
@@ -26,7 +27,7 @@ const About = (): React.ReactElement => {
           fontWeight: "bold",
           textAlign: "center",
           letterSpacing: "0.1em",
-          WebkitTextStroke: "2px #013476",
+          WebkitTextStroke: "2px #013476"
           
         }}>
           CONHEÇA O DAAL
@@ -35,8 +36,7 @@ const About = (): React.ReactElement => {
 
 
       <div style={{
-        display: "block",
-        
+        marginTop: "10vh"
       }}>
         <Box sx={{
           color: "#339edd",
@@ -47,40 +47,99 @@ const About = (): React.ReactElement => {
         }}>
           O QUE É O DAAL?
         </Box>
-        <Divider sx={{ 
-          marginY: "0.5rem",
-          borderColor: "#013476",
-          borderWidth: "0.1rem",
-          width: "55vw"
-        }} />
-        
+        <Divider className="divider_subtitle"/>
         <Box sx={{
           display: "flex",
-          marginLeft: "5%",
-          marginRight: "5%"
-        }}>
+        }} className="container">
           <Box sx={{
-            width: "50vw"
+            width: "45vw"
           }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iste odio eligendi laboriosam minus repellendus reiciendis, hic quis quia magni et tenetur sit id officia quae ipsam quod enim a!Lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, tempore. Consequatur nam molestiae delectus itaque nihil id adipisci quos! Repellat vitae, qui nam asperiores eaque facere ipsum molestiae alias eum!
-            texto
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, optio, aperiam error minima mollitia odio hic dicta, esse in necessitatibus dolorum nam ipsum quidem. Delectus doloremque iste porro explicabo odit adipisci ipsa totam tempora, eius placeat error quisquam cupiditate exercitationem tempore similique autem repudiandae pariatur, aspernatur quia voluptatem nobis id quis. Rem animi distinctio voluptates incidunt fugit eum similique quos. Animi hic expedita in corporis! Veritatis non provident quos enim neque consectetur doloribus vero labore exercitationem aliquam. Sapiente quia maiores dolores! Aliquam sequi tempore eum nam asperiores dolores magni voluptatibus aspernatur unde ullam, tenetur nisi facilis nobis in repellat eveniet provident facere vel illo consectetur
           </Box>
           <Box sx={{
             display: "flex",
             justifyContent: "flex-end",
-            alignItems: "right",
+            alignItems: "center",
             flex: "1"
           }}>
-            <img src="src/images/logo_daal.jpeg" alt="logo_daal" style={{
+            <img src="src/images/logo_daal2.0.png" alt="logo_daal" style={{
               width: "300px",
               height: "300px",
             }} />
           </Box>
         </Box>
+      </div>
+      <div>
+        <Box sx={{
+            color: "#339edd",
+            fontFamily: "Poppins",
+            fontWeight: "bold",
+            marginLeft: "5%",
+            fontSize: "1.5rem"
+          }}>
+            GESTÃO ATUAL DO DAAL
+        </Box>
+        <Divider className="divider_subtitle"/>
+        <Box className="container flex-row"
+          sx={{flexWrap: "wrap"}}>
 
+            <MemberProfile 
+              imgAltText="Andriel_Vinicius"
+              imgSrc="#"
+              role="1º Diretor geral"
+              name="Andriel Vinicius"
+            />
+            <MemberProfile 
+              imgAltText="Emilly_Miller"
+              imgSrc="#"
+              role="2º Diretora geral"
+              name="Emilly Miller"
+            />
+            <MemberProfile 
+              imgAltText="Murilo_Antonio"
+              imgSrc="#"
+              role="Secretário"
+              name="Murilo Antonio"
+            />
+            <MemberProfile 
+              imgAltText="Dinorah_de_Farias"
+              imgSrc="#"
+              role="Direção de Finanças"
+              name="Dinorah de Farias"
+            />
+            <MemberProfile 
+              imgAltText="Giliardo_Júlio"
+              imgSrc="#"
+              role="Direção de Ensino e Capacitação"
+              name="Giliardo Júlio"
+            />
+            <MemberProfile 
+              imgAltText="Clara_Virgínia"
+              imgSrc="#"
+              role="Diretora de Mulheres"
+              name="Clara Virgínia"
+            />
+            <MemberProfile 
+              imgAltText="Lucas_Vinicius"
+              imgSrc="#"
+              role="Direção de Cultura, Diversidade e Eventos"
+              name="Lucas Vinicius"
+            />
+            <MemberProfile 
+              imgAltText="Flawbert_Lorran"
+              imgSrc="#"
+              role="Direção de Comunicação"
+              name="Flawbert Lorran"
+            />
+            <MemberProfile 
+              imgAltText="Ramon_Jales"
+              imgSrc="#"
+              role="Direção de Lazer, Saúde e Bem-Estar"
+              name="Ramon Jales"
+            />
+        </Box>
       </div>
     </div>
-
   );
 };
 
