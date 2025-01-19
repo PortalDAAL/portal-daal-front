6ba43baae9ba6ef696b59692362c8ac7cd4903e8
leirdan <uouleirdan@gmail.com>
2025-01-19 20:46:51 -0300

    feat: melhorando pagina de login

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -1,10 +1,10 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Typography, useTheme } from "@mui/material";
 import LoginForm from "../../components/forms/login-form";
 import LoginImg from "../../images/login_img.png";
-import "./styles.css";
-import { texts } from "../../constants";
+import "./Login.styles.css";
 
 const Login = (): React.ReactElement => {
+  const theme = useTheme();
   return (
     <Box
       sx={{
@@ -26,23 +26,44 @@ const Login = (): React.ReactElement => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          borderWidth: "4px",
+          borderStyle: "solid",
+          borderColor: theme.palette.secondary.main,
+          borderRadius: 4,
+          paddingY: "2rem",
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.primary.contrastText,
+          paddingX: "1rem",
         }}
       >
         <Typography
-          variant="h3"
+          variant="h1"
+          color="inherit"
+          gutterBottom
           sx={{
-            marginBottom: 3,
-            fontSize: 20,
-            color: texts.color.primary,
-            fontWeight: texts.bold,
+            fontWeight: 700,
+            fontSize: 36,
           }}
         >
-          {" "}
           LOGIN
         </Typography>
-        <Card sx={{ maxWidth: "40rem", minWidth: "25rem" }}>
+        <Card
+          elevation={0}
+          sx={{
+            maxWidth: "25rem",
+            backgroundColor: theme.palette.primary.main,
+          }}
+        >
           <CardContent sx={{ marginTop: 2 }}>
-            <LoginForm />
+            <LoginForm
+              formTheme={{
+                backgroundColor: theme.palette.primary.contrastText,
+                buttonColor: theme.palette.primary.contrastText,
+                textColor: theme.palette.primary.contrastText,
+                labelColor: theme.palette.primary.main,
+                buttonTextColor: theme.palette.primary.main,
+              }}
+            />
           </CardContent>
         </Card>
       </Box>
